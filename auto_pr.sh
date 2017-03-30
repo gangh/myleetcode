9303dc38d9ded725b76547929cc1dee2b727c2b1
gangh <gsteel@foxmail.com>
2017-03-30 12:35:11 +0000

    auto refresh leetcode

--- auto_pr.sh
+++ auto_pr.sh
@@ -4,6 +4,4 @@ git add .
 git commit -m "auto refresh leetcode"
 git push
 hub pull-request -m "auto pr for refresh leetcode pull request from vps "
-git checkout master
-git checkout -b "new-branch-refresh-leetcode"
 git pull
