c933c5e611d348ae8d9fafd827728941cff55a62
Hua Jigang <me@huajigang.com>
2019-04-04 22:20:22 +0800

    del rubbish

--- auto_pr.sh
+++ auto_pr.sh
@@ -7,6 +7,6 @@ git add .
 git commit -m "auto refresh leetcode"
 git push
 echo "push success"
-hub pull-request -m "auto pr for refresh leetcode pull request from vps "
+hub pull-request -m "auto pr for refresh solutions from vps "
 echo "pr success"
 git pull
